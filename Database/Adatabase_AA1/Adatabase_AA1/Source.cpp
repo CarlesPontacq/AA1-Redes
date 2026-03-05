b30021ded6886b0244f0cc82036bdda7182ea6b2
CarlesPontacq <carles.pontacq@enti.cat>
2026-03-05 13:27:39 +0100

    Access to database

--- Database/Adatabase_AA1/Adatabase_AA1/Source.cpp
+++ Database/Adatabase_AA1/Adatabase_AA1/Source.cpp
@@ -4,31 +4,65 @@
 #include "mysql_connection.h"
 #include <cppconn/driver.h>
 #include <cppconn/exception.h>
+#include <cppconn/prepared_statement.h>
+#include <cppconn/resultset.h>
 
 #define SERVER "127.0.0.1:3306"
 #define USERNAME "root"
 #define PASSWORD "enti"
+#define DATABASE "Videogame"
+
+
+void ConnectDatabase(sql::Driver*& driver, sql::Connection*& con) {
+    try {
+        driver = get_driver_instance();
+        con = driver->connect(SERVER, USERNAME, PASSWORD);
+        std::cout << "Connection done." << std::endl;
+    }
+    catch (sql::SQLException e) {
+        std::cout << "Could not connect to server. Error message: " << e.what() << std::endl;
+    }
+}
+
+void DisconnectDatabase(sql::Connection* con) {
+    con->close();
+
+    if (con->isClosed()) {
+        std::cout << "Connection close." << std::endl;
+        delete con;
+    }
+}
+
+void GetAllUsers(sql::Connection* con) {
+    try {
+        sql::Statement* stmt = con->createStatement();
+        sql::ResultSet* res = stmt->executeQuery("SELECT username FROM users");
+
+        std::cout << "Users in the database:" << std::endl;
+
+        while (res->next()) {
+            std::cout << res->getString("username") << std::endl;
+        }
+
+        delete res;
+        delete stmt;
+    }
+    catch (sql::SQLException& e) {
+        std::cout << "Error while fetching users: " << e.what() << std::endl;
+    }
+}
 
 void main()
 {
     sql::Driver* driver;
     sql::Connection* con;
 
-    try
-    {
-        driver = get_driver_instance();
-        con = driver->connect(SERVER, USERNAME, PASSWORD);
-        std::cout << "Connection done." << std::endl;
+    ConnectDatabase(driver, con);
 
-        con->close();
-        std::cout << "Connection close." << std::endl;
-    }
-    catch (sql::SQLException e)
-    {
-        std::cout << "Could not connect to server. Error message: " << e.what() << std::endl;
-        system("pause");
-        exit(1);
-    }
+    con->setSchema(DATABASE);
+    GetAllUsers(con);
 
-    delete con;
+    DisconnectDatabase(con);
+
+    system("pause");
 }
---
+++ Database/HeidiSQLConsultas/Create_database.sql
@@ -0,0 +1,13 @@
+-- CREATE DATABASE Videogame;
+USE Videogame;
+
+-- CREATE TABLE users(
+	-- id INT AUTO_INCREMENT PRIMARY KEY,
+	-- username VARCHAR(50),
+	-- PASSWORD VARCHAR(255)
+-- );
+
+INSERT INTO users (username, PASSWORD) VALUES
+('Richard', 'suspenderAlumnos'),
+('Radev', 'salvarAlumnos'),
+('Edu', 'pas123');
