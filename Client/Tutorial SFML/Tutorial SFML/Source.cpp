5a4d44d9900315bf0197ed1828d1d02040b43357
CarlesPontacq <carles.pontacq@enti.cat>
2026-03-09 11:05:09 +0100

    Added server and client SFML projects

---
+++ Client/Tutorial SFML/Tutorial SFML/Source.cpp
@@ -0,0 +1,18 @@
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <string>
+
+#define SERVER_PORT 55000
+const sf::IpAddress SERVER_IP = sf::IpAddress(127, 0, 0, 1);
+
+void main()
+{
+	sf::TcpSocket socket;
+
+	if (socket.connect(SERVER_IP, SERVER_PORT) != sf::Socket::Status::Done) {
+		std::cerr << "Error al conectar con el servidor" << std::endl;
+	}
+	else {
+		std::cout << "Conectado al servidor" << std::endl;
+	}
+}
---
+++ Server/Tutorial SFML/Tutorial SFML/Source.cpp
@@ -0,0 +1,31 @@
+#include <SFML/Network.hpp>
+#include <iostream>
+#include <string>
+
+#define LISTENER_PORT 55000
+
+void main()
+{
+    sf::TcpListener listener;
+    sf::TcpSocket client;
+
+    bool closeServer = false;
+
+    //listener.setBlocking(false);
+
+    if (listener.listen(LISTENER_PORT) != sf::Socket::Status::Done) {
+        std::cerr << "Error al intentar escuchar en el puerto " << LISTENER_PORT << std::endl;
+    }
+
+    while (!closeServer) {
+        std::cout << "Esperando conexion..." << std::endl;
+        if (listener.accept(client) == sf::Socket::Status::Done) {
+            std::cout << "Cliente conectado desde " << client.getRemoteAddress().value() << std::endl;
+        }
+        else {
+            std::cerr << "Error al aceptar la conexion" << std::endl;
+        }
+
+    }
+
+}
