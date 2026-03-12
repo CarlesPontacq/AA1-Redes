b7f5c544fda8259126fbd9a817a795865883f857
CarlesPontacq <carles.pontacq@enti.cat>
2026-03-12 09:56:17 +0100

    Made different packets types

--- Client/Tutorial SFML/Tutorial SFML/Source.cpp
+++ Client/Tutorial SFML/Tutorial SFML/Source.cpp
@@ -5,6 +5,31 @@
 #define SERVER_PORT 55000
 const sf::IpAddress SERVER_IP = sf::IpAddress(127, 0, 0, 1);
 
+enum tipoPaquete { HANDSHAKE, LOGIN, MOVIMIENTO };
+
+sf::Packet& operator>>(sf::Packet& packet, tipoPaquete& tipo) {
+	int temp;
+	packet >> temp;
+	tipo = static_cast<tipoPaquete>(temp);
+
+	return packet;
+}
+
+void Handshake(sf::Packet data) {
+	std::string receiveMessage;
+	data >> receiveMessage;
+
+	std::cout << "Mensaje Handshake enviado del servidor: " << receiveMessage << std::endl;
+}
+
+void Login(sf::Packet data) {
+	std::string receiveMessage;
+	data >> receiveMessage;
+
+	std::cout << "Mensaje Login enviado del servidor: " << receiveMessage << std::endl;
+}
+
+
 void main()
 {
 	sf::TcpSocket socket;
@@ -14,5 +39,29 @@ void main()
 	}
 	else {
 		std::cout << "Conectado al servidor" << std::endl;
+
+		sf::Packet packet;
+
+
+		if (socket.receive(packet) == sf::Socket::Status::Done) {
+			tipoPaquete tipo;
+			packet >> tipo;
+
+			switch (tipo) {
+			case HANDSHAKE:
+				Handshake(packet);
+				break;
+			case LOGIN:
+				Login(packet);
+				break;
+			case MOVIMIENTO:
+				break;
+			}
+
+			packet.clear();
+		}
+		else {
+			std::cerr << "Error al recibir el mensaje del servidor" << std::endl;
+		}
 	}
 }
--- Server/Tutorial SFML/Tutorial SFML/Source.cpp
+++ Server/Tutorial SFML/Tutorial SFML/Source.cpp
@@ -1,11 +1,16 @@
 #include <SFML/Network.hpp>
 #include <iostream>
 #include <string>
+#include <time.h>
 
 #define LISTENER_PORT 55000
 
 void main()
 {
+    srand(time(NULL));
+
+    enum tipoPaquete {HANDSHAKE, LOGIN, MOVIMIENTO};
+
     sf::TcpListener listener;
     sf::TcpSocket client;
 
@@ -21,6 +26,30 @@ void main()
         std::cout << "Esperando conexion..." << std::endl;
         if (listener.accept(client) == sf::Socket::Status::Done) {
             std::cout << "Cliente conectado desde " << client.getRemoteAddress().value() << std::endl;
+
+            sf::Packet packet;
+            std::string message = "Hola cliente";
+
+            int tempRandom = rand() % 2;
+
+            switch (tempRandom)
+            {
+            case 0:
+                packet << tipoPaquete::HANDSHAKE << message;
+                break;
+            case 1:
+                packet << tipoPaquete::LOGIN << message;
+                break;
+            }
+
+
+            if (client.send(packet) == sf::Socket::Status::Done) {
+                std::cout << "Mensaje enviado: " << message << std::endl;
+                packet.clear();
+            }
+            else {
+                std::cerr << "Error al enviar el mensaje al cliente" << std::endl;
+            }
         }
         else {
             std::cerr << "Error al aceptar la conexion" << std::endl;
