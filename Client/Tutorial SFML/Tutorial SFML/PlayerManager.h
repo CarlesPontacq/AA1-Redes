070daeb6de18ec3a12202a189936d610df8a1e5b
eugenivasiliev <eugeni.vasiliev@enti.cat>
2026-04-18 19:07:00 +0200

    Added text for visuals

--- Client/Tutorial SFML/Tutorial SFML/PlayerManager.h
+++ Client/Tutorial SFML/Tutorial SFML/PlayerManager.h
@@ -9,15 +9,17 @@ public:
 	PlayerManager(User user, User otherUsers[PLAYER_COUNT - 1]) {
 		for (int index = 0; index < PLAYER_COUNT; ++index)
 			if (user.userIndex == index)
-				players[index] = Player(index);
+				players[index] = Player(user);
 			else for (int othersIndex = 0; othersIndex < PLAYER_COUNT - 1; ++othersIndex) 
 					if (otherUsers[othersIndex].userIndex == index) 
-						players[index] = Player(index);
+						players[index] = Player(otherUsers[othersIndex]);
 	}
 
 	void render(sf::RenderWindow& window) {
-		for (Player player : players)
+		for (Player player : players) {
 			window.draw(player.rectangle);
+			window.draw(player.nick);
+		}
 	}
 };
 
