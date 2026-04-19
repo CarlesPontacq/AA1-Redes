1504dd215e681c76243230db647d6a1a610293e1
erica <erica.baltar@enti.cat>
2026-04-19 21:53:31 +0200

    Ranking Scene UI

--- Client/Tutorial SFML/Tutorial SFML/Ranking.h
+++ Client/Tutorial SFML/Tutorial SFML/Ranking.h
@@ -3,40 +3,88 @@
 #include "Object.h"
 #include "RankingStyle.h"
 #include "User.h"
-#include <iostream>
+#include <string>
 
 class Ranking : public Object
 {
 	sf::RectangleShape rectangle;
-	sf::Text text;
+	sf::RectangleShape accentBar;
+	sf::Text rankText;
+	sf::Text nickText;
+	sf::Text scoreText;
 
 public:
-	Ranking() : text(emptyFont) {}
+	Ranking() : rankText(emptyFont), nickText(emptyFont), scoreText(emptyFont) {}
 
-	Ranking(User user, sf::Font& font, int index) : text(font) {
-		rectangle.setPosition({
-			WINDOW_WIDTH * rankingsAnchorX,
-			WINDOW_HEIGHT * rankingsAnchorY + user.userIndex * (rankingsHeight + rankingsSeparation)
-			});
-		rectangle.setFillColor(rankingsBackground);
+	Ranking(User user, sf::Font& font, int index)
+		: rankText(font), nickText(font), scoreText(font)
+	{
+		const float posX = WINDOW_WIDTH * rankingsAnchorX;
+		const float posY = WINDOW_HEIGHT * rankingsAnchorY + index * (rankingsHeight + rankingsSeparation);
+
+		rectangle.setPosition({ posX, posY });
 		rectangle.setSize({ rankingsWidth, rankingsHeight });
+		rectangle.setFillColor(index < 3 ? rankingsTopThreeBackground : rankingsBackground);
+		rectangle.setOutlineThickness(2.f);
+		rectangle.setOutlineColor(rankingsOutlineColor);
 
-		text.setString(user.nickname + "             " + std::to_string(user.score));
+		accentBar.setPosition(rectangle.getPosition());
+		accentBar.setSize({ 8.f, rankingsHeight });
 
-		//Center text
-		text.setPosition({
-			rectangle.getPosition().x + rectangle.getSize().x / 2.0f - text.getCharacterSize() / 2.0f * text.getString().getSize() / 2.0f,
-			rectangle.getPosition().y + rectangle.getSize().y / 2.0f - text.getCharacterSize() / 2.0f
-			});
+		if (index == 0)
+			accentBar.setFillColor(sf::Color(255, 210, 90));
+		else if (index == 1)
+			accentBar.setFillColor(sf::Color(180, 190, 210));
+		else if (index == 2)
+			accentBar.setFillColor(sf::Color(205, 140, 95));
+		else
+			accentBar.setFillColor(rankingAccentXColor);
 
-		text.setCharacterSize(rankingsCharacterSize);
-		text.setStyle(rankingsTextStyle);
-		text.setFillColor(sf::Color::Black);
+		// Rank number
+		rankText.setString("#" + std::to_string(index + 1));
+		rankText.setCharacterSize(rankingsCharacterSize - 4);
+		rankText.setStyle(rankingsTextStyle);
+		rankText.setFillColor(rankingsIndexColor);
+		{
+			sf::FloatRect bounds = rankText.getLocalBounds();
+			rankText.setPosition({
+				rectangle.getPosition().x + 22.f,
+				rectangle.getPosition().y + (rectangle.getSize().y - bounds.size.y) / 2.f - bounds.position.y - 2.f
+				});
+		}
+
+		// Nickname
+		nickText.setString(user.nickname);
+		nickText.setCharacterSize(rankingsCharacterSize);
+		nickText.setStyle(rankingsTextStyle);
+		nickText.setFillColor(rankingsTextColor);
+		{
+			sf::FloatRect bounds = nickText.getLocalBounds();
+			nickText.setPosition({
+				rectangle.getPosition().x + 85.f,
+				rectangle.getPosition().y + (rectangle.getSize().y - bounds.size.y) / 2.f - bounds.position.y - 2.f
+				});
+		}
+
+		// Score
+		scoreText.setString(std::to_string(user.score));
+		scoreText.setCharacterSize(rankingsCharacterSize);
+		scoreText.setStyle(rankingsTextStyle);
+		scoreText.setFillColor(rankingsTextColor);
+		{
+			sf::FloatRect bounds = scoreText.getLocalBounds();
+			scoreText.setPosition({
+				rectangle.getPosition().x + rectangle.getSize().x - bounds.size.x - bounds.position.x - 24.f,
+				rectangle.getPosition().y + (rectangle.getSize().y - bounds.size.y) / 2.f - bounds.position.y - 2.f
+				});
+		}
 	}
 
-	void render(sf::RenderWindow& window) {
+	void render(sf::RenderWindow& window) override {
 		window.draw(rectangle);
-		window.draw(text);
+		window.draw(accentBar);
+		window.draw(rankText);
+		window.draw(nickText);
+		window.draw(scoreText);
 	}
 };
-
